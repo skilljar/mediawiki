7370c6da1f630aec75098288cbeb5e71e39dd843
Aaron Fischer <aaron@skilljar.com>
2018-07-07 14:14:17 -0700

    short urls

--- LocalSettings.php
+++ LocalSettings.php
@@ -27,7 +27,8 @@ $wgMetaNamespace = "Skilljar";
 ## For more information on customizing the URLs
 ## (like /w/index.php/Page_title to /wiki/Page_title) please see:
 ## https://www.mediawiki.org/wiki/Manual:Short_URL
-$wgScriptPath = "";
+$wgScriptPath = "/w";
+$wgArticlePath = "/wiki/$1";
 
 ## The protocol and server name to use in fully-qualified URLs
 $wgServer = "https://skilljar-wiki.herokuapp.com";
